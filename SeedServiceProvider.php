7c6304579b76ceb9b8a9c41950410c41b8c2a967
Huy Nguyen <40920254+wowcee@users.noreply.github.com>
2018-11-21 08:18:44 +0700

    Added getClassesFromFile() method
    Use getClassesFromFile() method to get full name of class (include namespace) in each seed file.

--- SeedServiceProvider.php
+++ SeedServiceProvider.php
@@ -66,30 +66,56 @@ class SeedServiceProvider extends ServiceProvider
     /**
      * Register seeds.
      *
-     * @param  string  $seeds_path
+     * @param string  $seeds_path
      * @return void
      */
     protected function addSeedsFrom($seeds_path)
     {
-        $file_names = glob("$seeds_path/*.php");
+        $file_names = glob( $seeds_path . '/*.php');
         foreach ($file_names as $filename)
         {
-            $php_code = file_get_contents($filename);
+            $classes = $this->getClassesFromFile($filename);
+            foreach ($classes as $class) {
+                Artisan::call('db:seed', [ '--class' => $class, '--force' => '' ]);
+            }
+        }
+    }
 
-            // Get name of all class has in the file.
-            $classes = array();
-            $tokens = token_get_all($php_code);
-            $count = count($tokens);
-            for ($i = 2; $i < $count; $i++) {
-                if ($tokens[$i - 2][0] == T_CLASS && $tokens[$i - 1][0] == T_WHITESPACE && $tokens[$i][0] == T_STRING) {
-                    $class_name = $tokens[$i][1];
+    /**
+     * Get full class names declared in the specified file.
+     *
+     * @param string $filename
+     * @return array an array of class names.
+     */
+    private function getClassesFromFile(string $filename) : array
+    {
+        // Get namespace of class (if vary)
+        $namespace = "";
+        $lines = file($filename);
+        $namespaceLines = preg_grep('/^namespace /', $lines);
+        if (is_array($namespaceLines)) {
+            $namespaceLine = array_shift($namespaceLines);
+            $match = array();
+            preg_match('/^namespace (.*);$/', $namespaceLine, $match);
+            $namespace = array_pop($match);
+        }
+
+        // Get name of all class has in the file.
+        $classes = array();
+        $php_code = file_get_contents($filename);
+        $tokens = token_get_all($php_code);
+        $count = count($tokens);
+        for ($i = 2; $i < $count; $i++) {
+            if ($tokens[$i - 2][0] == T_CLASS && $tokens[$i - 1][0] == T_WHITESPACE && $tokens[$i][0] == T_STRING) {
+                $class_name = $tokens[$i][1];
+                if ($namespace !== "") {
+                    $classes[] = $namespace . "\\$class_name";
+                } else {
                     $classes[] = $class_name;
                 }
             }
-
-            foreach ($classes as $class) {
-                Artisan::call('db:seed', [ '--class' => "Core\\database\\seeds\\$class", '--force' => true ]);
-            }
         }
+
+        return $classes;
     }
 }
