8b8a9781ad82f125fba39fccdf69e7848a838790
Adiwit Co., Ltd <50860673+adiwit-co-th@users.noreply.github.com>
2020-05-09 20:02:05 +0700

    Undo whitespace changes to original `Artisan:call`

--- SeedServiceProvider.php
+++ SeedServiceProvider.php
@@ -78,7 +78,7 @@ class SeedServiceProvider extends ServiceProvider
             foreach ($classes as $class) {
                 echo "\033[1;33mSeeding:\033[0m {$class}\n";
                 $startTime = microtime(true);
-                Artisan::call('db:seed', ['--class' => $class, '--force' => '']);
+                Artisan::call('db:seed', [ '--class' => $class, '--force' => '' ]);
                 $runTime = round(microtime(true) - $startTime, 2);
                 echo "\033[0;32mSeeded:\033[0m {$class} ({$runTime} seconds)\n";
             }
